1e49d56644b820f98889af287f70f1b454932b50
apple-x-co <apple.x.co@gmail.com>
2022-08-20 10:27:44 +0900

    refactor

--- src/main.zig
+++ src/main.zig
@@ -10,11 +10,12 @@ pub fn main() anyerror!void {
     _ = args.nextPosix().?; // skip program name
 
     const stdout = std.io.getStdOut();
+    const stderr = std.io.getStdErr();
     
     var arg = args.nextPosix();
     if (arg == null) {
         const stdin = std.io.getStdIn();
-        jq(stdin, stdout, allocator) catch |err| {
+        jq(stdin, stdout, stderr, allocator) catch |err| {
             std.log.warn("error reading stdin : {}", .{err});
         };
 
@@ -28,60 +29,36 @@ pub fn main() anyerror!void {
 
         const file = try std.fs.cwd().openFile(arg.?, .{ .read = true, .write = false });
         defer file.close();
-        jq(file, stdout, allocator) catch |err| {
+        jq(file, stdout, stderr, allocator) catch |err| {
             std.log.warn("error reading file '{s}': {}", .{ arg.?, err });
         };
     }
 }
 
-fn jq(in: std.fs.File, out: std.fs.File, allocator: std.mem.Allocator) anyerror!void {
-    _ = in.reader();
+fn jq(in: std.fs.File, out: std.fs.File, errOut: std.fs.File, allocator: std.mem.Allocator) anyerror!void {
+    const reader = in.reader();
     const writer = out.writer();
+    const errWriter = errOut.writer();
 
     var parser = std.json.Parser.init(allocator, false);
     defer parser.deinit();
     
-    const payload =
-        \\{
-        \\    "vals": {
-        \\        "testing": 1,
-        \\        "production": 42
-        \\    },
-        \\    "uptime": 9999
-        \\}
-    ;
+    var payload = try reader.readAllAlloc(allocator, std.math.maxInt(usize));
+    defer allocator.free(payload);
+
     var parsed = parser.parse(payload) catch |err| {
-        std.debug.print("error: {s}", .{@errorName(err)});
+        try errWriter.print("error: {s}\n", .{@errorName(err)});
+
         return;
     };
     defer parsed.deinit();
 
-    //std.debug.print("{s}\n", .{parsed.root.dump()});
-
     try parsed.root.jsonStringify(std.json.StringifyOptions{
         .whitespace = std.json.StringifyOptions.Whitespace{}
     }, writer);
-    std.debug.print("\n", .{});
-
-    // const reader = in.reader();
-    // const writer = out.writer();
-
-    // var buf: [std.mem.page_size]u8 = undefined;
-    // var i: u32 = 1;
-    // while (true) : (i += 1) {
-    //     var line = reader.readUntilDelimiterOrEof(buf[0..buf.len], '\n') catch null;
-    //     if (line == null) {
-    //         break;
-    //     }
-
-    //     if (try regex.partialMatch(line.?)) {
-    //         try writer.print("{}:{s}\n", .{ i, line.? });
-    //     }
-    // } else |err| {
-    //     std.log.warn("{}", .{err});
-    // }
+    try writer.print("\n", .{});
 }
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
-}
+// test "basic test" {
+//     try std.testing.expectEqual(10, 3 + 7);
+// }
